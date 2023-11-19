ea2bcdb258ee0f184d5d17222e4df472ab2cbc91
Senna van Hoek <27216694+sennavanhoek@users.noreply.github.com>
2023-11-19 12:49:13 +0100

    Initial id_cores.py

---
+++ id_cores.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from pyocd.core.helpers import ConnectHelper
+
+
+with ConnectHelper.session_with_chosen_probe() as session:
+    target = session.target
+    for c in session.target.cores:
+        core = session.target.cores[c]
+        core_fam = core.name
+        core_rev = F"r{core.cpu_revision}p{core.cpu_patch}"
+        fpu = "with FPU" if core.has_fpu else "witout FPU"
+        print(core_fam, core_rev, fpu)
