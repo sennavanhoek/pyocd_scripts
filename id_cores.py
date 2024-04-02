3bf479da5f5bc1f1d32c2a4faf055fd87c7ce853
Senna van Hoek <27216694+sennavanhoek@users.noreply.github.com>
2024-04-02 12:46:40 +0200

    fixed typo in id_cores.py

--- id_cores.py
+++ id_cores.py
@@ -8,5 +8,5 @@ with ConnectHelper.session_with_chosen_probe() as session:
         core = session.target.cores[c]
         core_fam = core.name
         core_rev = F"r{core.cpu_revision}p{core.cpu_patch}"
-        fpu = "with FPU" if core.has_fpu else "witout FPU"
+        fpu = "with FPU" if core.has_fpu else "without FPU"
         print(core_fam, core_rev, fpu)
